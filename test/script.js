8a71b1006190331cf926866a9a30e0a863f02652
jihane zergui <zerguijihane8@gmail.com>
2026-08-19 23:44:12 +0200

    Add initial HTML structure, CSS reset, and root styles
    - Created index.html with basic layout including header, main sections, and footer.
    - Added reset.css to normalize default browser styles.
    - Introduced root.css for defining color variables and basic styling for body and sections.
    - Created a test HTML file with linked JavaScript for future functionality.
    - Added script.js with commented-out code snippets for various JavaScript exercises.

---
+++ test/script.js
@@ -0,0 +1,103 @@
+// const x = +prompt("enter the  x");
+// const y = +prompt("enter the  y");
+
+// const sum = x + y;
+
+// console.log("The sum of " + x + " and " + y + " is: " + sum);
+
+// const x = +prompt("enter the x");
+// const y = +prompt("enter the y");
+// const sum = x * y;
+// console.log("The result of multiplying " + x + " and " + y + " is: " + sum);
+
+// const num1 = +prompt("enter a num")
+// if(num1%2===0){
+// console.log("the number is even")
+// }
+// else{
+//     console.log("the number is odd")
+
+// const start = +prompt("enter a starting in interval ");
+// const end = +prompt("enter a ending in interval");
+
+// for (var i = start; i <= end;i++)  {
+// if (i%2 !==0){
+//     console.log(i);
+// }
+
+// }
+
+// const number = +prompt("enter a number")
+
+
+// for (var i = 1; i<=10;i++) {
+// // var mult = number * i;
+// console.log(number +" * " + i +" = " + mult )
+// }
+
+// const num1 = +prompt("enter a number");
+// var sum = 0;
+
+// while(num1>0){
+// sum = sum + i;
+// i++;
+// }
+// console.log("sum = " + sum);
+
+// const produit = [5,10,15,20];
+// const sum = produit.reduce((totale , any)=>totale*any ,1);
+// console.log(sum);
+
+// const playlist = ["song1", "song2", "song3", "song4"];
+// console.log(playlist);
+// console.log(playlist[0]);
+// console.log(playlist[1]);
+// console.log(playlist[2]);
+// console.log(playlist[3]);
+
+// for (var i = 0; iindex < playlist.length; index++) {
+//     console.log(playlist[index]);
+// }   
+
+// for (const song of playlist) {
+//     console.log(song);
+// } 
+// playlist.push("song5");
+// console.log(playlist);
+
+// playlist.pop();
+// console.log(playlist);
+
+// playlist.unshift("song0");
+// console.log(playlist);
+
+// const index = prompt("enter a song name");
+
+
+// const student = {
+// name: "jihane",
+// level: 3,
+// grades :{
+//  math: 95
+//  science: 88
+//  french: 97
+// },
+// };
+
+// function averageGrade(student){
+//     var grades = Object.values(student.grades);
+//     console.log(grades);
+//     var sum = grades.reduce((total, grade) => total + grade, 0);
+//     const avg = sum / grades.length;
+//     return avg.toFixed(2);
+// }
+
+// consoe.log(student);
+// console.log(student.name);
+// console.log(student.level);
+// console.log(student.grades.math);
+// console.log(student.grades.history);
+// console.log(student.grades.french);
+// // averageGrade(student);
+// const studentAverage = averageGrade(student.subjects);
+// console.log(studentAvg);
